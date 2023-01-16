47265462336fb14958320cdce4186d7fddda932e
kimkuns <kimkuns98@gmail.com>
2023-01-16 15:28:06 +0900

    ReUpload

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {});
+  const port = process.env.PORT || 3000;
+  console.log("Server running at http://15.165.75.7:" + port + "/");
+  await app.listen(port);
 }
 bootstrap();
